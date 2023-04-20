4f0debdc8a3d3900ff8b7ac436be6515b527196e
Tony Blard <tonyblard55@gmail.com>
2023-04-20 15:00:58 +0200

    Create User in AppFixtures.php

---
+++ project/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+
+    public function __construct(private readonly UserPasswordHasherInterface $hasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail("tonyblard55@gmail.com")->setRoles([])->setPlainPassword("tada");
+        $user->setPassword($this->hasher->hashPassword($user,$user->getPlainPassword()));
+        $user->eraseCredentials();
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
