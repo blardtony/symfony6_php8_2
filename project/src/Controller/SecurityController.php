163f6a89eacc946273b3ff3152edddb96e08ef04
Tony Blard <tonyblard55@gmail.com>
2023-04-20 14:29:53 +0200

    Create logout method

--- project/src/Controller/SecurityController.php
+++ project/src/Controller/SecurityController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,11 +12,21 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 #[Route('/', name: 'security_')]
 class SecurityController extends AbstractController
 {
-    #[Route('login', name: 'login')]
+    #[Route('login', name: 'login', methods: [Request::METHOD_GET, Request::METHOD_POST])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         return $this->render('security/login.html.twig', [
             'error' => $authenticationUtils->getLastAuthenticationError()
         ]);
     }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('logout', name: 'logout', methods: [Request::METHOD_GET])]
+    public function logout(): Exception
+    {
+        throw new Exception('logout() should never be reached');
+    }
+
 }
