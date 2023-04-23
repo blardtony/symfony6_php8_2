ee3d20b7d5bd12c128ae3578e353481a31fd259d
Tony Blard <tonyblard55@gmail.com>
2023-04-23 15:53:08 +0200

    Create AccountNotVerifiedAuthenticationException Exception when user try to log in with an unverified account

---
+++ project/src/Exception/Security/AccountNotVerifiedAuthenticationException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Exception\Security;
+
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+class AccountNotVerifiedAuthenticationException extends AuthenticationException
+{
+
+}
